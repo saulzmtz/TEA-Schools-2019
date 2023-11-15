578fc663a7a7448fe48faddffb225b227c9bbc83
saúl z mtz <97975590+szmtz@users.noreply.github.com>
2023-11-15 21:18:36 +0000

    Display basic demographic data

--- script.js
+++ script.js
@@ -56,3 +56,44 @@ function updateSuggestions(inputValue, suggestionsElementId) {
         suggestionsElement.appendChild(div);
     });
 }
+
+
+function displaySchoolData(school1Data, school2Data) {
+    // Check if both school data are available
+    if (!school1Data || !school2Data) {
+        console.error('Missing data for one or both schools');
+        return;
+    }
+
+    // Display basic information for School 1
+    const school1Demo = document.getElementById('school1-demographics');
+    school1Demo.innerHTML = `
+        <h3>${school1Data.Campus}</h3>
+        <p>District: ${school1Data.District}</p>
+        <p>County: ${school1Data.County}</p>
+        <p>${school1Data.Region}</p>
+    `;
+
+    // Display basic information for School 2
+    const school2Demo = document.getElementById('school2-demographics');
+    school2Demo.innerHTML = `
+        <h3>${school2Data.Campus}</h3>
+        <p>District: ${school2Data.District}</p>
+        <p>County: ${school2Data.County}</p>
+        <p>${school2Data.Region}</p>
+    `;
+}
+
+document.getElementById('compare-button').addEventListener('click', () => {
+    const school1Name = document.getElementById('school1-search').value.trim();
+    const school2Name = document.getElementById('school2-search').value.trim();
+
+    const school1Data = schoolsData.find(school => school.Campus.toLowerCase() === school1Name.toLowerCase());
+    const school2Data = schoolsData.find(school => school.Campus.toLowerCase() === school2Name.toLowerCase());
+
+    if (school1Data && school2Data) {
+        displaySchoolData(school1Data, school2Data);
+    } else {
+        console.error('One or both schools not found');
+    }
+});

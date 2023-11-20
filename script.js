d49fb069ed6830e6ce1cf0fa99b3e434cd306651
saúl z mtz <97975590+szmtz@users.noreply.github.com>
2023-11-20 22:51:18 +0000

    Update color palette

--- script.js
+++ script.js
@@ -75,7 +75,7 @@ function createPieChart(chartId, data, label) {
             datasets: [{
                 label: label,
                 data: [data, 100 - data],
-                backgroundColor: ['#36a2eb', '#ddd'],
+                backgroundColor: ['#00a1f1', '#ddd'],
                 borderWidth: 1
             }]
         },
@@ -140,18 +140,18 @@ function createBarChart(chartId, scores) {
                 label: 'scores',
                 data: Object.values(scores),
                 backgroundColor: [
-                    'rgba(54, 162, 235, 0.6)',
-                    'rgba(255, 206, 86, 0.6)',
-                    'rgba(75, 192, 192, 0.6)',
-                    'rgba(153, 102, 255, 0.6)',
+                    '#00a1f1',
+                    '#7cbb00',
+                    '#ffbb00',
+                    '#f65314',
                     'rgba(255, 159, 64, 0.6)',
                     'rgba(255, 99, 132, 0.6)'
                 ],
                 borderColor: [
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)',
-                    'rgba(153, 102, 255, 1)',
+                    '#00a1f1',
+                    '#7cbb00',
+                    '#ffbb00',
+                    '#f65314',
                     'rgba(255, 159, 64, 1)',
                     'rgba(255, 99, 132, 1)'
                 ],
